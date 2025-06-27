549431876a7f24481fc50fc5353beddce4e6538e
Javier Garcia <garciaja@cern.ch>
2025-06-27 22:28:36 +0200

    cleaner

--- ScoutingParking/SctParAnalyzer/plugins/SctParLooper.cc
+++ ScoutingParking/SctParAnalyzer/plugins/SctParLooper.cc
@@ -45,44 +45,6 @@
 #include "L1Trigger/L1TGlobal/interface/L1TGlobalUtil.h"
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 
-template<class T>
-const T& getObject(const edm::Event& ev, const edm::EDGetTokenT<T>& token) {
-  edm::Handle<T> handle;
-  ev.getByToken(token, handle);
-  return *handle;
-}
-
-template <typename T, typename Compare>
-std::vector<std::size_t> sort_permutation(const std::vector<T>& vec, Compare compare) {
-  std::vector<std::size_t> p(vec.size());
-  std::iota(p.begin(), p.end(), 0);
-  std::sort(p.begin(), p.end(), [&](std::size_t i, std::size_t j){ return compare(vec[i], vec[j]); });
-  return p;
-}
-
-template <typename T>
-void apply_permutation_in_place(std::vector<T>& vec, const std::vector<std::size_t>& p) {
-  std::vector<bool> done(vec.size());
-  for (std::size_t i=0; i<vec.size(); ++i) {
-    if (done[i])
-      continue;
-    done[i] = true;
-    std::size_t prev_j = i;
-    std::size_t j = p[i];
-    while (i != j) {
-      //std::swap(vec[prev_j], vec[j]);
-      T tmp = vec[prev_j];
-      //bool tmp = vec[prev_j];
-      vec[prev_j] = vec[j];
-      vec[j] = tmp;
-
-      done[j] = true;
-      prev_j = j;
-      j = p[j];
-    }
-  }
-}
-
 //Header
 class SctParLooper : public edm::one::EDAnalyzer<> {
   public:
